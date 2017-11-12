2c27b79c0de32598f98e5eb9b640d770deaa56e9
Ricardo Cruz Leyva <rcl6395@gmail.com>
2017-11-12 15:47:01 -0500

    Avances del catálogo de puestos

--- app/Http/Controllers/Configuracion/Catalogo/PuestoController.php
+++ app/Http/Controllers/Configuracion/Catalogo/PuestoController.php
@@ -8,15 +8,30 @@ use Illuminate\Support\Facades\Input;
 use App\Http\Controllers\BaseController;
 use Validator;
 
-use DataTable;
+use App\Model\MDepartamento;
+use App\Model\MPuesto;
+
+use Yajra\DataTables\DataTables;
 
 class PuestoController extends BaseController{
 
-	public function index(){
+	public function index(DataTables $datatables){
 
+		$columns = ['#','id','nombre','departamento','fecha','opciones'];
 
+		$data['table'] = $datatables->getHtmlBuilder()
+							->columns($columns)	
+							->parameters([
+								'dom'       => 'Bfrtip',
+								'buttons'   => ['reload','export'],
+								'saveState' => true
+							])
+							->ajax([
+								'method'    => 'POST',
+								'url'       => url('configuracion/catalogos/puestos/post-data')
+							]);
 
-		return view('');
+		return view('Configuracion.Catalogo.Puesto.indexPuesto')->with($data);
 	}
 
 	public function manager(Request $request){
@@ -25,10 +40,57 @@ class PuestoController extends BaseController{
 
 	public function postDataTable(){
 
+		$data = MPuesto::selectRaw('PUES_PUESTO as id, PUES_NOMBRE as nombre, PUES_CREATED_AT as fecha, PUES_ENABLED')
+						->where('PUES_DELETED',0);
+
+		return DataTables::of($data)
+			->addColumn('#', function($query){
+				return '<div class="custom-controls-stacked">
+		                    <label class="custom-control custom-checkbox">
+		                        <input type="checkbox" class="custom-control-input" id="remember" name="remember" value="'.$query->PUES_PUESTO.'">
+		                        <span class="custom-control-indicator"></span>
+		                    </label>
+		                </div>';
+			})
+			->addColumn('departamento',function($query){
+				return 'nombre del departamento';
+			})
+			->addColumn('opciones',function($query){
+					$buttons = '';
+
+					$buttons .= '<button type="button" class="btn btn-xs btn-rounded btn-noborder btn-outline-primary" onclick="hTipoDocumento.delete('.$query->PUES_PUESTO.')"><i class="fa fa-eye"></i></button>';
+
+					$buttons .= '<button type="button" class="btn btn-xs btn-rounded btn-noborder btn-outline-success" onclick="hTipoDocumento.delete('.$query->PUES_PUESTO.')"><i class="fa fa-pencil"></i></button>';
+				
+					if($query->PUES_ENABLED == 1){
+						$buttons .= '<button type="button" class="btn btn-xs btn-rounded btn-noborder btn-outline-danger" onclick="hTipoDocumento.disable('.$query->PUES_PUESTO.')"><i class="fa fa-level-down"></i></button>';
+					}else{
+						$buttons .= '<button type="button" class="btn btn-xs btn-rounded btn-noborder btn-outline-success" onclick="hTipoDocumento.enabled('.$query->PUES_PUESTO.')"><i class="fa fa-level-up"></i></button>';
+					}
+
+					$buttons .= '<button type="button" class="btn btn-xs btn-rounded btn-noborder btn-outline-danger" onclick="hTipoDocumento.delete('.$query->PUES_PUESTO.')"><i class="fa fa-trash"></i></button>';
+					
+					return $buttons;
+			},1)
+			->removeColumn('PUES_ENABLED')
+			->rawColumns(['#','opciones'])
+			->make();
 	}
 
 	public function formPuesto(){
 		try{
+
+			$data = [
+				'title'         => 'Nuevo puesto',
+				'url_send_form' => '',
+				'form_id'       => '',
+			];
+
+			$data['departamentos'] = MDepartamento::select('DEPA_DEPARTAMENTO','DEPA_NOMBRE')
+									->pluck('DEPA_NOMBRE','DEPA_DEPARTAMENTO')
+									->toArray();
+
+			return view('Configuracion.Catalogo.Puesto.formPuesto')->with($data);
 
 		}catch(Exception $error){
 
---
+++ resources/views/Configuracion/Catalogo/Puesto/formPuesto.blade.php
@@ -0,0 +1,71 @@
+<div class="block-header bg-primary-dark">
+    <h3 class="block-title">{{ $title or '' }}</h3>
+    <div class="block-options">
+        <button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
+            <i class="si si-close"></i>
+        </button>
+    </div>
+</div>
+<div class="block-content">
+    {{ Form::open(['url'=>$url_send_form,'method'=>'POST','id'=>$form_id]) }}
+        <div class="form-group row">
+            <label class="col-sm-3 col-form-label" for="nombre">Nombre</label>
+            <div class="col-sm-9">
+            	{{ Form::text('nombre',null,['id'=>'nombre','class'=>'form-control','placeholder'=>'Nombre del puesto','autofocus']) }}
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-3 col-form-label" for="departamento">Departamento</label>
+            <div class="col-sm-9">
+            	{{ Form::select('departamento',$departamentos,null,['id'=>'departamento','class'=>'form-control']) }}
+            </div>
+        </div>
+	{{ Form::close() }}
+</div>
+
+<script type="text/javascript">
+	
+	$.extend(AppForm, new function(){
+
+		this.init = function(){
+			$this = this
+			this.context = $('div.modal.fade#modal-{{ $form_id }}')
+			this.form = this.context.find('form')
+			this.btnOk = this.context.find('#btn-ok')
+			this.btnCancel = this.context.find('#btn-cancel')
+
+			this.formSubmit(this.form)
+
+	        this.btnOk.on('click', function(e){
+	        	$this.submit()
+	        });
+		}	
+		
+		this.submitHandler = function(form){
+			if(!$(form).valid()) return false;
+			App.ajaxRequest({
+				url  : $(form).attr('action'),
+				data : $(form).serialize(),
+				before : function(){
+					Codebase.blocks( AppForm.context.find('div.modal-content'), 'state_loading')
+				},
+				success : function(data){
+					AppForm.btnCancel.click()
+				}
+			})
+		}
+
+		this.rules = function(){
+			return {
+				nombre : { required : true, maxlength : 255 }
+			}
+		}
+
+		this.messages = function(){
+			return {
+				nombre : { required : 'Introduzca un nombre' }
+			}
+		}
+	}).init()
+
+</script>
