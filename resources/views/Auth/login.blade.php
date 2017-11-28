7b7e15fb3d0759c6cec6d640b3bb0251c156323d
Ricardo Cruz Leyva <ricardo_cruz6395@hotmail.com>
2017-11-28 15:25:27 -0500

    Fix

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -65,7 +65,7 @@
                                     <div class="form-group row">
                                         <div class="col-12">
                                             <div class="form-material floating open">
-                                                {{ Form::text('username',old('username'),['id'=>'username','class'=>'form-control text-center','autofocus']) }}
+                                                {{ Form::text('username',old('username'),['id'=>'username','class'=>'form-control text-center','autofocus','autocomplete'=>'off']) }}
                                                 {{ Form::label('username','Usuario') }}
                                             </div>
                                         </div>
