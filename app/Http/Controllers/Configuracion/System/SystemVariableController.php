7cb2ddf28f0168be25e70ebf59cd13beaa8bc7c9
Ricardo Cruz Leyva <ricardo_cruz6395@hotmail.com>
2018-12-04 19:32:42 -0500

    fix

--- app/Http/Controllers/Configuracion/System/SystemVariableController.php
+++ app/Http/Controllers/Configuracion/System/SystemVariableController.php
@@ -86,7 +86,7 @@ class SystemVariableController extends BaseController
 
             $variables = MSystemConfig::all();
 
-            foreach ([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20] as $key) {
+            foreach ([1,2,3,4,5,6,9,10,11,12,13,14,15,17,18,19,20] as $key) {
                 $variable = $variables->find($key);
 
                 $variable->SYCO_VALOR = $request->get('var' . $key);
